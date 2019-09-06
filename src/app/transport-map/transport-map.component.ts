c8f2f709901ea6af8ddf8e65057a94e89c25f786
Pavan Bhatta <n10239111@qut.edu.au>
2019-09-06 14:36:35 +1000

    Incorporating Brisbane map into the project

--- src/app/transport-map/transport-map.component.ts
+++ src/app/transport-map/transport-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-transport-map',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransportMapComponent implements OnInit {
 
+  public map: any;
   constructor() { }
 
   ngOnInit() {
+    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set('pk.eyJ1IjoicGF2YW50ZWphOTAiLCJhIjoiY2p6eXl6ZzhiMDV3cDNvcDd6Nmd3bWtmNyJ9.Q7ZMA251fPPlfRenu1V-Gg');
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [153.0294284, -27.469861],
+      zoom: 11.50
+    });
   }
 
 }
